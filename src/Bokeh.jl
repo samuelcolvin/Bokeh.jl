98466cc890641a6ca9a65badb0a4122bd7abc857
Samuel Colvin <s@muelcolvin.com>
2014-06-12 20:36:56 +0100

    all basic objects implemented

--- src/Bokeh.jl
+++ src/Bokeh.jl
@@ -2,7 +2,6 @@ module Bokeh
 	include("objects.jl")
 	using JSON
 
-
 	function obdict(ob::PlotObject, doc::UUID)
 		d = Dict{String, VALUE_TYPES}()
 		d["id"] = string(ob.uuid)
@@ -12,7 +11,9 @@ module Bokeh
 		attrs["doc"] = string(doc)
 		extra_attrs = typeof(ob).names
 		for name in extra_attrs[2:end]
-			attrs[string(name)] = ob.(name)
+			key = string(name)
+			# key = begingswith(key, "_") ? key[2:end] : key
+			attrs[key] = ob.(name)
 		end
 		d["attributes"] = attrs
 		d
@@ -21,20 +22,43 @@ module Bokeh
 	function test()
 		obs = Any[]
 		pid= uuid4()
+
 		p = PlotContext(pid)
 		doc = uuid4()
 		push!(obs, obdict(p, doc))
-		axis = Axis(0, pid)
-		axis = 
-		push!(obs, obdict(axis, doc))
+
+		axis0 = Axis(0, pid)
+		push!(obs, obdict(axis0, doc))
+
+		axis1 = Axis(1, pid)
+		push!(obs, obdict(axis1, doc))
+
 		column_names = String["x", "y"]
 		data = Dict{String, Array{Number, 1}}()
 		data["x"] = [1, 2, 3, 4, 5]
 		data["y"] = [1, 4, 9, 16, 25]
 		column = ColumnDataSource(column_names, data)
 		push!(obs, obdict(column, doc))
-		dr1 = DataRange1d(column.uuid, String["y"])
-		push!(obs, obdict(dr1, doc))
+
+		dr1y = DataRange1d(column.uuid, String["y"])
+		push!(obs, obdict(dr1y, doc))
+
+		dr1x = DataRange1d(column.uuid, String["x"])
+		push!(obs, obdict(dr1x, doc))
+
+		glyph = Glyph(column.uuid, dr1x.uuid, dr1y.uuid)
+		push!(obs, obdict(glyph, doc))
+
+		renderers = [
+			("Glyph", glyph.uuid),
+			("Axis", axis0.uuid),
+			("Axis", axis1.uuid),
+		]
+		plot = Plot(column.uuid,
+					dr1x.uuid,
+					dr1y.uuid,
+					renderers
+					)
 		JSON.print(obs, 2)
 	end
 end
--- src/objects.jl
+++ src/objects.jl
@@ -12,12 +12,8 @@ const VALUE_TYPES = Any # Union(Dict, Array, String, Number, Bool, Nothing, UUID
 
 function typeiddict(name::String, id::UUID)
 	Dict{String, VALUE_TYPES}([
-		("type", name),
-		("id", string(id))])
-end
-
-function plotdict(plotid::UUID)
-	typeiddict("plot", plotid)
+		"type" => name,
+		"id" => string(id)])
 end
 
 type ColumnDataSource <: PlotObject
@@ -27,22 +23,12 @@ type ColumnDataSource <: PlotObject
 	discrete_ranges::Dict{String, VALUE_TYPES}
 	cont_ranges::Dict{String, VALUE_TYPES}
 	data::Dict{String, Array{Number, 1}}
-	ColumnDataSource(column_names::Array{String, 1},
-					 selected::Array{Any, 1},
-					 discrete_ranges::Dict{String, VALUE_TYPES},
-					 cont_ranges::Dict{String, VALUE_TYPES},
-					 data::Dict{String, Array{Number, 1}}) = new(
-						 uuid4(), 
-						 column_names,
-						 selected,
-						 discrete_ranges, 
-						 cont_ranges, 
-						 data)
 end
 
 function ColumnDataSource(column_names::Array{String, 1}, data::Dict{String, Array{Number, 1}})
-	ColumnDataSource(column_names,
-					 Any[],
+	ColumnDataSource(uuid4(),
+					 column_names,
+					 VALUE_TYPES[],
 					 Dict{String, VALUE_TYPES}(), 
 					 Dict{String, VALUE_TYPES}(), 
 					 data)
@@ -51,15 +37,13 @@ end
 type DataRange1d <: Range
 	uuid::UUID
 	sources::Dict{String, VALUE_TYPES}
-	DataRange1d(sources::Dict{String, VALUE_TYPES}) = new(
-		uuid4(), sources)
 end
 
-function DataRange1d(coldataid::UUID, columns::Array{String, 1})
+function DataRange1d(coldata_id::UUID, columns::Array{String, 1})
 	sources = Dict{String, VALUE_TYPES}()
 	sources["columns"] = columns
-	sources["source"] = typeiddict("ColumnDataSource", coldataid)
-	DataRange1d(sources)
+	sources["source"] = typeiddict("ColumnDataSource", coldata_id)
+	DataRange1d(uuid4(), sources)
 end
 
 
@@ -69,35 +53,89 @@ type Axis <: Renderer
 	formatter::Any
 	ticker::Any
 	plot::Dict{String, VALUE_TYPES}
-	Axis(dimension::Int, formatter, ticker, plot::Dict{String, VALUE_TYPES}) = new(
-		uuid4(),
-		dimension, 
-		formatter, 
-		ticker,
-		plot)
 end
 
 function Axis(dimension::Int, plotid::UUID)
-	Axis(dimension, nothing, nothing, plotdict(plotid))
+	Axis(uuid4(), dimension, nothing, nothing, typeiddict("plot", plotid))
 end
 
 type Glyph <: Renderer
 	uuid::UUID
-	Glyph() = new(uuid4())
+	data_source::Dict{String, VALUE_TYPES}
+	server_data_source::Any
+	xdata_range::Dict{String, VALUE_TYPES}
+	ydata_range::Dict{String, VALUE_TYPES}
+	glyphspec::Dict{String, VALUE_TYPES}
+end
+
+function Glyph(coldata_id::UUID, 
+			   xrange_id::UUID, 
+			   yrange_id::UUID, 
+			   alpha::Float64=1.0,
+			   _type::String="line", 
+			   colour::String="blue")
+	glyphspec = Dict{String, VALUE_TYPES}([
+		"line_color" => ["value" => colour],
+		"line_width" => ["units" => "data", "field" => "line_width"],
+		"line_alpha" => ["units" => "data", "field" => alpha],
+		"y" => ["units" => "data", "field" => "y"],
+		"x" => ["units" => "data", "field" => "x"],
+		"type" => "line"
+		])
+	Glyph(coldata_id, xrange_id, yrange_id, glyphspec)
+end
+
+function Glyph(coldata_id::UUID, xrange_id::UUID, yrange_id::UUID, glyphspec::Dict{String, VALUE_TYPES})
+	data_source = typeiddict("ColumnDataSource", coldata_id)
+	server_data_source = nothing
+	xdata_range = typeiddict("DataRange1d", xrange_id)
+	ydata_range = typeiddict("DataRange1d", yrange_id)
+	Glyph(uuid4(), data_source, server_data_source, xdata_range, ydata_range, glyphspec)
 end
 
 type Plot <: PlotObject
 	uuid::UUID
-	Plot() = new(uuid4())
+	title::String
+	Tools::Array{VALUE_TYPES, 1}
+	outer_height::Int
+	canvas_height::Int
+	outer_width::Int
+	canvas_width::Int
+	x_range::Dict{String, VALUE_TYPES}
+	y_range::Dict{String, VALUE_TYPES}
+	renderers::Array{VALUE_TYPES, 1}
+	data_sources::Array{VALUE_TYPES, 1}
+end
+
+function Plot(coldata_id::UUID,
+			  xrange_id::UUID, 
+			  yrange_id::UUID,
+			  renderers::Array{(ASCIIString, UUID),1},
+			  title::String="Bokeh Plot",
+			  height::Int=600,
+			  width::Int=600)
+	data_sources = [typeiddict("ColumnDataSource", coldata_id)]
+	xdata_range = typeiddict("DataRange1d", xrange_id)
+	ydata_range = typeiddict("DataRange1d", yrange_id)
+	renderers = [typeiddict(name, id) for (name, id) in renderers]
+	Plot(uuid4(),
+		 title,
+		 VALUE_TYPES[],
+		 height,
+		 height,
+		 width,
+		 width,
+		 xdata_range,
+		 ydata_range,
+		 renderers,
+		 data_sources)
 end
 
 type PlotContext <: PlotObject
 	uuid::UUID
 	children::Array{Dict{String, VALUE_TYPES}, 1}
-	PlotContext(children::Array{Dict{String, VALUE_TYPES}, 1}) = new(uuid4(),children)
 end
 
 function PlotContext(plotid::UUID)
-	PlotContext([plotdict(plotid)])
+	PlotContext(uuid4(),[typeiddict("plot", plotid)])
 end
-
