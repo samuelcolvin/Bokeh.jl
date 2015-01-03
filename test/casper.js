4fc1a86b96a95ccb4a8dcff6bb91b141af303866
Samuel Colvin <s@muelcolvin.com>
2015-01-03 18:22:47 +0000

    add Bokeh console output to casperjs

--- test/casper.js
+++ test/casper.js
@@ -1,7 +1,10 @@
 casper.test.begin("simple.js rendering", 1, function(test) {
+  casper.on('remote.message', function(message) {
+    this.echo(message);
+  });
   casper.start('test/_testing/simple.html', function() {
   	// we should probably set page titles but we don't yet.
-    this.echo('title: "' + this.getTitle() + '"');
+    //this.echo('title: "' + this.getTitle() + '"');
     // once we do we should add a test.
   });
 
